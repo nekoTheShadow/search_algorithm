3446d8d50aded6002bb1eda9d99324eb3ab3351e
neko_the_shadow <h.nakamura0903@gmail.com>
2023-03-09 23:51:51 +0900

    3.4.2 Chokudaiサーチの実装

---
+++ src/_03_OnePlayerGame/_06_ChokudaiSearch/TestRandomScore.java
@@ -0,0 +1,26 @@
+package _03_OnePlayerGame._06_ChokudaiSearch;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class TestRandomScore {
+    public static void main(String[] args) {
+        testAiScore(100);
+    }
+    
+    public static void testAiScore(int gameNumber) {
+        Random random = ThreadLocalRandom.current();
+        double scoreMean = 0;
+        for (int i = 0; i < gameNumber; i++) {
+            MazeState state = new MazeState(random.nextLong());
+            while (!state.isDone()) {
+                state.advance(MazeState.chokudaiSearchAction(state, 1, MazeState.END_TURN, 2));
+            }
+            int score = state.gameScore;
+            scoreMean += score;
+        }
+        scoreMean /= gameNumber;
+        System.out.println("Score:\t"+scoreMean);
+    }
+}
---
+++ src/_03_OnePlayerGame/_07_ChokudaiSearchWithTime/MazeState.java
@@ -0,0 +1,279 @@
+package _03_OnePlayerGame._07_ChokudaiSearchWithTime;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MazeState implements Comparable<MazeState> {
+    public static void main(String[] args) {
+        playGame(121321);
+    }
+    
+    public static void playGame(long seed) {
+        MazeState state = new MazeState(seed);
+        System.out.println(state);
+        while (!state.isDone()) {
+            state.advance(greedyAction(state));
+            System.out.println(state);
+        }
+        
+    }
+    
+    public static int randomAction(MazeState state) {
+        Random random = ThreadLocalRandom.current();
+        List<Integer> actions = state.legalActions();
+        return actions.get(random.nextInt(actions.size()));
+    }
+    
+    public static int greedyAction(MazeState state) {
+        List<Integer> legalActions = state.legalActions();
+        int bestScore = -INF;
+        int bestAction = -1;
+        for (int action : legalActions) {
+            MazeState nowState = new MazeState(state);
+            nowState.advance(action);
+            nowState.evaluatedScore();
+            if (nowState.evaluatedScore > bestScore) {
+                bestScore = nowState.evaluatedScore;
+                bestAction = action;
+            }
+        }
+        return bestAction;
+    }
+    
+    public static int beamSearchAction(MazeState state, int beamWidth, int beamDepth) {
+        PriorityQueue<MazeState> nowBeam = new PriorityQueue<>();
+        MazeState bestState = null;
+        
+        nowBeam.add(state);
+        for (int t = 0; t < beamDepth; t++) {
+            PriorityQueue<MazeState> nextBeam = new PriorityQueue<>();
+            for (int i = 0; i < beamWidth; i++) {
+                if (nowBeam.isEmpty()) {
+                    break;
+                }
+                
+                MazeState nowState = nowBeam.remove();
+                List<Integer> legalActions = nowState.legalActions();
+                for (int action : legalActions) {
+                    MazeState nextState = new MazeState(nowState);
+                    nextState.advance(action);
+                    nextState.evaluatedScore();
+                    if (t == 0) {
+                        nextState.firstAction = action;
+                    }
+                    nextBeam.add(nextState);
+                }
+            }
+            
+            nowBeam = nextBeam;
+            bestState = nowBeam.peek();
+            if (bestState.isDone()) {
+                break;
+            }
+        }
+        return bestState.firstAction;
+    }
+    
+    public static int chokudaiSearchActionWithTimeThreshold(MazeState state, int beamWidth, int beamDepth, long timeThreshold) {
+        TimeKeeper timeKeeper = new TimeKeeper(timeThreshold);
+        
+        List<PriorityQueue<MazeState>> beam = new ArrayList<>();
+        for (int i = 0; i < beamDepth+1; i++) {
+            beam.add(new PriorityQueue<>());
+        }
+        beam.get(0).add(state);
+        
+        for (int cnt = 0; ; cnt++) {
+            for (int t = 0; t < beamDepth; t++) {
+                PriorityQueue<MazeState> nowBeam = beam.get(t);
+                PriorityQueue<MazeState> nextBeam = beam.get(t+1);
+                for (int i = 0; i < beamWidth; i++) {
+                    if (nowBeam.isEmpty()) {
+                        break;
+                    }
+                    
+                    MazeState nowState = nowBeam.peek();
+                    if (nowState.isDone()) {
+                        break;
+                    }
+                    
+                    nowBeam.remove();
+                    List<Integer> legalActions = nowState.legalActions();
+                    for (int action : legalActions) {
+                        MazeState nextState = new MazeState(nowState);
+                        nextState.advance(action);
+                        nextState.evaluatedScore();
+                        if (t==0) {
+                            nextState.firstAction = action;
+                        }
+                        nextBeam.add(nextState);
+                    }
+                }
+            }
+            if (timeKeeper.isTimeOver()) {
+                break;
+            }
+        }
+        
+        for (int t = beamDepth; t >= 0; t--) {
+            PriorityQueue<MazeState> nowBeam = beam.get(t);
+            if (!nowBeam.isEmpty()) {
+                return nowBeam.peek().firstAction;
+            }
+        }
+        return -1;
+    }
+
+    public static int chokudaiSearchAction(MazeState state, int beamWidth, int beamDepth, int beamNumber) {
+        List<PriorityQueue<MazeState>> beam = new ArrayList<>();
+        for (int i = 0; i < beamDepth+1; i++) {
+            beam.add(new PriorityQueue<>());
+        }
+        beam.get(0).add(state);
+        
+        for (int cnt = 0; cnt < beamNumber; cnt++) {
+            for (int t = 0; t < beamDepth; t++) {
+                PriorityQueue<MazeState> nowBeam = beam.get(t);
+                PriorityQueue<MazeState> nextBeam = beam.get(t+1);
+                for (int i = 0; i < beamWidth; i++) {
+                    if (nowBeam.isEmpty()) {
+                        break;
+                    }
+                    
+                    MazeState nowState = nowBeam.peek();
+                    if (nowState.isDone()) {
+                        break;
+                    }
+                    
+                    nowBeam.remove();
+                    List<Integer> legalActions = nowState.legalActions();
+                    for (int action : legalActions) {
+                        MazeState nextState = new MazeState(nowState);
+                        nextState.advance(action);
+                        nextState.evaluatedScore();
+                        if (t==0) {
+                            nextState.firstAction = action;
+                        }
+                        nextBeam.add(nextState);
+                    }
+                }
+            }
+        }
+        
+        for (int t = beamDepth; t >= 0; t--) {
+            PriorityQueue<MazeState> nowBeam = beam.get(t);
+            if (!nowBeam.isEmpty()) {
+                return nowBeam.peek().firstAction;
+            }
+        }
+        return -1;
+    }
+
+    
+    
+    private static int H = 30;
+    private static int W = 30;
+    public static int END_TURN = 100;
+    private static int[] dx = new int[] {1,-1,0, 0};
+    private static int[] dy = new int[] {0, 0,1,-1};
+    private static int INF = Integer.MAX_VALUE;
+    
+    private int[][] points;
+    private int turn;
+    public Coord character;
+    public int gameScore;
+    public int evaluatedScore;
+    public int firstAction;
+    
+    public MazeState(long seed) {
+        this.turn = 0;
+        this.gameScore = 0;
+        this.firstAction = -1;
+        
+        Random random = new Random(seed);
+        this.character = new Coord(random.nextInt(W), random.nextInt(H));
+        this.points = new int[H][W];
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                if (y == this.character.y && x == this.character.x) {
+                    this.points[y][x] = 0;;
+                } else {
+                    this.points[y][x] = random.nextInt(10);
+                }
+            }
+        }
+    }
+    
+    public MazeState(MazeState other) {
+        this.points = new int[H][W];
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                this.points[y][x] = other.points[y][x];
+            }
+        }
+        this.turn = other.turn;
+        this.character = new Coord(other.character.x, other.character.y);
+        this.gameScore = other.gameScore;
+        this.evaluatedScore = other.evaluatedScore;
+        this.firstAction = other.firstAction;
+    }
+    
+    public boolean isDone() {
+        return this.turn == END_TURN;
+    }
+    
+    public void advance(int action) {
+        this.character.x += dx[action];
+        this.character.y += dy[action];
+        int point = this.points[this.character.y][this.character.x];
+        if (point > 0) {
+            this.gameScore += point;
+            this.points[this.character.y][this.character.x] = 0;
+        }
+        this.turn++;
+    }
+    
+    public List<Integer> legalActions() {
+        List<Integer> actions = new ArrayList<>();
+        for (int action = 0; action < 4; action++) {
+            int ty = this.character.y + dy[action];
+            int tx = this.character.x + dx[action];
+            if (0 <= ty && ty < H && 0 <= tx && tx < W) {
+                actions.add(action);
+            }
+        }
+        return actions;
+    }
+    
+    public void evaluatedScore() {
+        this.evaluatedScore = this.gameScore;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("turn :\t%d%n".formatted(this.turn));
+        sb.append("score:\t%d%n".formatted(this.gameScore));
+        for (int h = 0; h < H; h++) {
+            for (int w = 0; w < W; w++) {
+                if (this.character.y == h && this.character.x == w) {
+                    sb.append("@");
+                } else if (this.points[h][w] > 0) {
+                    sb.append(this.points[h][w]);
+                } else {
+                    sb.append(".");
+                }
+            }
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(MazeState other) {
+        return Integer.compare(other.evaluatedScore, this.evaluatedScore);
+    }
+}
