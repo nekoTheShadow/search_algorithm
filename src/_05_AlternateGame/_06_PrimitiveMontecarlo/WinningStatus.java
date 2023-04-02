0a99398ddb01bee9f4e16f1949d765446948358f
neko_the_shadow <h.nakamura0903@gmail.com>
2023-04-02 20:43:34 +0900

    5.5.2 原始モンテカルロ法の実装

---
+++ src/_05_AlternateGame/_06_PrimitiveMontecarlo/WinningStatus.java
@@ -0,0 +1,8 @@
+package _05_AlternateGame._06_PrimitiveMontecarlo;
+
+public enum WinningStatus {
+    WIN,
+    LOSE,
+    DRAW,
+    NONE,
+}
---
+++ src/_05_AlternateGame/_07_PrimitiveMontecarloPlayoutNumber/WinningStatus.java
@@ -0,0 +1,8 @@
+package _05_AlternateGame._07_PrimitiveMontecarloPlayoutNumber;
+
+public enum WinningStatus {
+    WIN,
+    LOSE,
+    DRAW,
+    NONE,
+}
